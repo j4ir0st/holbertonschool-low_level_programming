9952d5664c60576d8f958010a211ffce639f0a30
j4ir0st <jairocast2@gmail.com>
2021-09-30 13:06:25 -0700

    prints a message with printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+/**
+ * main - Prints a message with printf
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	printf ("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
