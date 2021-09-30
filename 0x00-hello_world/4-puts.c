ad805aa83c21254a1605e04ecb5f7a5931232346
j4ir0st <jairocast2@gmail.com>
2021-09-30 12:40:59 -0700

    prints a message to the user

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/**
+ * main - Prints a message to the user
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	print "\"Programming is like building a multilingual puzzle";
+}
