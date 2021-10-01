e4bf630b7627bcf14ef3cb179144da2754684fda
j4ir0st <jairocast2@gmail.com>
2021-10-01 14:07:36 -0700

    prints the alphabet except e and q

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char l = 'a';
+
+	do {
+		if (l != 'e' && l != 'q')
+		{
+			putchar(l);
+		}
+		l++;
+
+	} while (l <= 'z');
+
+	printf("\n");
+
+	return (0);
+}
