21a956c6521cac34698c4b04073ae5858f70988b
j4ir0st <jairocast2@gmail.com>
2021-10-01 13:32:40 -0700

    Prints the alfabeth

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char l = 'a';
+
+	do {
+		putchar(l);
+		l++;
+
+	}
+	while (l <= 'z');
+
+	printf("\n");
+
+	return (0);
+}
