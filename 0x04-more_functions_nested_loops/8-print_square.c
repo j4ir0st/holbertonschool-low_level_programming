d9bc405ec4905c42852b72420d67a04586b4d1d0
j4ir0st <jairocast2@gmail.com>
2021-10-11 10:49:59 -0700

    homework

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,29 @@
+#include <main.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+void print_square(int size)
+{
+	int x = 0;
+	int y = 0;
+
+	while (x < size)
+	{
+		while (y < size)
+		{
+			_putchar(35);
+			y++;
+		}
+		_putchar(10);
+		x++;
+	}
+	if (x <= 0)
+	{
+		_putchar(10);
+	}
+
+	return (0);
+}
---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int n = 1;
+
+	printf("%i", n);
+	n = 2;
+
+	while (n<=100)
+	{
+		if (n % 3 == 0 && n % 5 == 0)
+		{
+			printf(" FizzBuzz");
+		}
+		else if (n %3 == 0)
+		{
+			printf(" Fizz");
+		}
+		else if (n % 5 == 0)
+		{
+			printf(" Buzz");
+		}
+		else
+		{
+			printf("%i", n);
+		}
+		n++;
+	}
+	printf("\n");
+	return (0);
+}
