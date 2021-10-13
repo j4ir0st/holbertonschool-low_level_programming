815d291a4086fc63d676481b6b1e06daab68ef53
j4ir0st <jairocast2@gmail.com>
2021-10-12 22:20:34 -0700

    pointer used for reset to 98

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,11 +1,13 @@
 #include "main.h"
 
 /**
- * main - Entry point
- *
- * Return: Always 0 (Success)
+ * reset_to_98 - Recieves an adress and set it to 98
+ * @n: adress of the variable
+ * Return: void
  */
+
 void reset_to_98(int *n)
 {
+	/** n - set the adress to 98 */
 	*n = 98;
 }
