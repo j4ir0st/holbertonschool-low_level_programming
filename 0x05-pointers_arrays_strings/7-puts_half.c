9bc0d1bab7d6b0d24206cb921c8941a51883d6d5
j4ir0st <jairocast2@gmail.com>
2021-10-13 23:52:52 -0700

    prints the half of a string

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+/**
+ * _puts - Entry point
+ * @str: char variable
+ *
+ */
+void puts_half(char *str)
+{
+	int n = 0;
+
+	while (str[n] != 0)
+	{
+		n++;
+	}
+	if (n % 2 != 0)
+	{
+		n = (n - 1) / 2;
+	}
+	else
+	{
+		n = n / 2;
+	}
+	while (str[n] != 0)
+	{
+		printf("%c", str[n]);
+		n++;
+	}
+	printf("\n");
+}
