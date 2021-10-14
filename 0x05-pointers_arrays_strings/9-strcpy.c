d2859377719f3ca37b93267a257daa80cc6ed252
j4ir0st <jairocast2@gmail.com>
2021-10-14 08:07:35 -0700

    copies the str to dest

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,20 @@
+/**
+ * *_strcpy - copies the string pointed to src
+ * @dest: char address argument
+ * @src: char argument
+ * Return: returns dest argument
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	int n = 0;
+
+	do
+	{
+		dest[n] = src[n];
+		n++;
+	}
+	while (src[n] != 0);
+
+	return (src);
+}
