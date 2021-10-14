b1e64203aefde74f3b702777ea1906f0a713dbd4
j4ir0st <jairocast2@gmail.com>
2021-10-13 20:50:27 -0700

    prints a string in reverse

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+/**
+ * print_rev - Entry point
+ * @s: char variable
+ * Return: Always 0 (Success)
+ */
+
+void print_rev(char *s)
+{
+	int m = 0;
+
+	while (s[m] != 0)
+	{
+		m++;
+	}
+
+	do
+	{
+		printf("%c", s[m]);
+		m--;
+
+	}while (m >= 0);
+	printf("\n");
+
+
+}
