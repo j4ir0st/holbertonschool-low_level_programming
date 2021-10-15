224ea2fc1c13266ab98a43b22d4b4dd3cdcebe32
j4ir0st <jairocast2@gmail.com>
2021-10-15 10:07:42 -0700

    compares two string ascii level

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,25 @@
+/**
+ * *_strcmp - compares two strings
+ * @s1: char argument
+ * @s2: char argument
+ */
+
+char _strcmp(char *s1, char *s2)
+{
+	int c = 0;
+	int v1, v2;
+	int dif = 0;
+
+	while (s1[c] && s2[c])
+	{
+		v1 = s1[c];
+		v2 = s2[c];
+		if (v1 != v2)
+		{
+			dif = v1 - v2;
+			break;
+		}
+		c++;
+	}
+	return (dif);
+}
