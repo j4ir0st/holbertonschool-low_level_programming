7475912a7b119aea1420f6c7102b5bfdfee43d66
j4ir0st <jairocast2@gmail.com>
2021-10-18 15:16:28 -0700

    locates a substring

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,40 @@
+/**
+ * *_strstr - locates a substring
+ * @haystack: char argument as pointer
+ * @needle: char argument
+ */
+
+char *_strstr(char *haystack, char *needle)
+{
+	int c = 0, len = 0;
+
+	while (needle[len])
+	{
+		len++;
+	}
+	while (*(haystack))
+	{
+		while (c < len)
+		{
+			if (*(haystack + c) == needle[c])
+			{
+				c++;
+			}
+			else
+			{
+				break;
+			}
+		}
+		if (*(haystack + c) == needle[c])
+		{
+			break;
+		}
+		c = 0;
+		haystack++;
+	}
+	if (*(haystack) == 0 && needle != 0)
+	{
+		haystack = 0;
+	}
+	return (haystack);
+}
