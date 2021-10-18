3a5cefd4d8b820b228b25b390ae8d97617bb3873
j4ir0st <jairocast2@gmail.com>
2021-10-18 14:07:59 -0700

    searches a string for any of a set of bytes

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,33 @@
+/**
+ * _strspn - gets the length of a prefix substring
+ * @s: char argument as pointer
+ * @accept: char argument
+ * Return: return pointer s
+ */
+char *_strpbrk(char *s, char *accept)
+{
+	int c = 0;
+
+	while (*(s))
+	{
+		while (accept[c])
+		{
+			if (*(s) == accept[c])
+			{
+				break;
+			}
+			c++;
+		}
+		if (*(s) == accept[c])
+		{
+			break;
+		}
+		c = 0;
+		s++;
+	}
+	if (*(s) == 0 && accept != 0)
+	{
+		s = 0;
+	}
+	return (s);
+}
