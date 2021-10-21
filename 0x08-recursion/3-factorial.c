992efa6b6794964a2c780db8c90091454a3f3067
j4ir0st <jairocast2@gmail.com>
2021-10-21 10:45:48 -0700

    returns the factorial of given number using recursive

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,20 @@
+/**
+ * _strlen_recursion - returns the lenght of a string using recursion
+ * @s: char argument
+ */
+
+int factorial(int n)
+{
+	if (n < 0)
+	{
+		return (-1);
+	}
+	else if (n == 1)
+	{
+		return (1);
+	}
+	else
+	{
+		return (n * factorial(n - 1));
+	}
+}
