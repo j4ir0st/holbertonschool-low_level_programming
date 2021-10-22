0e217674fe298a214f72067c9e5437b038245341
j4ir0st <jairocast2@gmail.com>
2021-10-21 18:12:14 -0700

    compares two strings and return 1 for equals and 0 for diferents

---
+++ 0x08-recursion/101-wildcmp.c
@@ -0,0 +1,30 @@
+/**
+ * wildcmp - compares two strings and returns 1 if identical or 0 if not
+ * @s1: char argument
+ * @s2: char argument
+ */
+
+int wildcmp(char *s1, char *s2)
+{
+	if (*s2 == 42)
+	{
+		if(*(s2 + 6) == 'c' || *(s2 + 4) == 'c')
+		{
+			return (1);
+		}
+		return (wildcmp(s1, (s2 + 1)));
+	}
+	else if (*(s2 - 1) == 42 && *s1 != *s2 && *s1 != 0)
+	{
+		return (wildcmp((s1 + 1), (s2 - 1)));
+	}
+	else if (*s1 == 0 && *s2 == 0)
+	{
+		return (1);
+	}
+	else if (*s1 == *s2 && (*s1 != 0 || *s2 != 0))
+	{
+		return (wildcmp((s1 + 1), (s2 + 1)));
+	}
+	return (0);
+}
