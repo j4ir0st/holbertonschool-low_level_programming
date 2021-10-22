d6f3e441e1e92b42d8039ef946067c3cccde5017
j4ir0st <jairocast2@gmail.com>
2021-10-22 15:43:07 -0700

    prints its name

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,9 +1,13 @@
 #include <stdio.h>
+
 /**
  * main - imput
- * Return: returns the length of argv
+ * @argc: int argument
+ * @argv: char argument
+ * Return: returns 0
  */
-int main (int argc,char *argv[])
+
+int main (int argc, char *argv[])
 {
 	printf("%s\n", argv[0]);
 	return (argc);
