2d076b1199da26b32a8056cd7cb074f65a10e501
j4ir0st <jairocast2@gmail.com>
2021-10-22 18:51:06 -0700

    prints the minimun number of coins to make change

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -2,8 +2,10 @@
 #include <stdlib.h>
 #include <string.h>
 
+int _change(int cents);
+
 /**
- * main - imput
+ * main - input
  * @argc: int argument
  * @argv: char argument
  * Return: returns 0
@@ -11,49 +13,22 @@
 
 int main(int argc, char *argv[])
 {
-	int d, c = 1;
+	int d, c = 0;
 	int cents;
 
 	if (argc == 2)
 	{
-		for (d = 0; argv[c][d]; d++)
+		for (d = 0; argv[1][d]; d++)
 		{
-			if (argv[c][d] < 48 || argv[c][d] > 57)
+			if ((argv[1][d] < 48 || argv[1][d] > 57) && argv[1][0] != 45)
 			{
 				printf("Error\n");
 				return (1);
 			}
 		}
-		cents = atoi(argv[c]);
-		while (cents > 0)
-		{
-			if (cents >= 25)
-			{
-				cents = cents - 25;
-				c++;
-			}
-			else if (cents >= 10)
-			{
-				cents = cents - 10;
-				c++;
-			}
-			else if (cents >= 5)
-			{
-				cents = cents - 5;
-				c++;
-			}
-			else if (cents >= 2)
-			{
-				cents = cents - 2;
-				c++;
-			}
-			else if (cents >= 1)
-			{
-				cents = cents - 1;
-				c++;
-			}
-		}
-		printf("%i\n", c - 1);
+		cents = atoi(argv[1]);
+		c = _change(cents);
+		printf("%i\n", c);
 	}
 	else
 	{
@@ -61,4 +36,43 @@ int main(int argc, char *argv[])
 		return (1);
 	}
 	return (0);
+}
+/**
+ * _change - imput
+ * @cents: int argument
+ * Return: returns cents
+ */
+int _change(int cents)
+{
+	int c = 0;
+
+	while (cents > 0)
+	{
+		if (cents >= 25)
+		{
+			cents = cents - 25;
+			c++;
+		}
+		else if (cents >= 10)
+		{
+			cents = cents - 10;
+			c++;
+		}
+		else if (cents >= 5)
+		{
+			cents = cents - 5;
+			c++;
+		}
+		else if (cents >= 2)
+		{
+			cents = cents - 2;
+			c++;
+		}
+		else if (cents >= 1)
+		{
+			cents = cents - 1;
+			c++;
+		}
+	}
+	return (c);
 }
