b49f853f39e8f16b06baec722f5d0b5b2d539561
j4ir0st <jairocast2@gmail.com>
2021-10-28 13:27:52 -0700

    concatenates two strings

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -30,39 +30,24 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	{
 		len2++;
 	}
-	if (n >= len2)
+	if(n >= len2)
 	{
-		str = malloc(sizeof(char) * (len1 + len2));
-		if (str == NULL)
-		{
-			return (NULL);
-		}
-		for (c = 0; c <= len1; c++)
-		{
-			*(str + c) = *(s1 + c);
-		}
-		for (d = 0; d <= len2; d++)
-		{
-			*(str + (c - 1) + d) = *(s2 + d);
-		}
-		*(str + (c - 1) + d) = '\0';
+		n = len2;
 	}
-	else
+	str = malloc(sizeof(char) * (len1 + n - 1));
+	if (str == NULL)
 	{
-		str = malloc(sizeof(char) * (len1 + n));
-		if (str == NULL)
-		{
-			return (NULL);
-		}
-		for (c = 0; c <= len1; c++)
-		{
-			*(str + c) = *(s1 + c);
-		}
-		for (d = 0; d <= n; d++)
-                {
-                        *(str + (c - 1) + d) = *(s2 + d);
-                }
-                *(str + (c - 1) + d) = '\0';
+		return (NULL);
+	}
+	for (c = 0; c <= len1; c++)
+	{
+		*(str + c) = *(s1 + c);
+	}
+	for (d = 0; d <= len2; d++)
+	{
+		*(str + (c - 1) + d) = *(s2 + d);
+	}
+	*(str + (c - 1) + d) = '\0';
 	}
 	return (str);
 }
