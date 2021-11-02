cc104eeb272b65f306b4355576b50d1424cbfb67
j4ir0st <jairocast2@gmail.com>
2021-11-02 13:54:56 -0700

    defines a new type of structs named dog

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef _DOG_H_
+#define _DOG_H_
+/**
+ * struct my_dog - Short description
+ * @name: First member
+ * @age: Second member
+ * @owner: Third member
+ *
+ * Description: Longer description
+ */
+
+struct dog {
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
