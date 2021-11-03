e7c95fe6c3b799c8d6596d96ce5b54f4ad633ee6
j4ir0st <jairocast2@gmail.com>
2021-11-03 13:39:08 -0700

    array iterator

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * array_iterator - entry point
+ * @array: array argument
+ * @size: size_t argument
+ * @action: function pointer
+ * Return: Nothing.
+ */
+
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	size_t c;
+
+	if (array == NULL)
+	{
+		return;
+	}
+	for(c = 0; c < size; c++)
+	{
+		action(array[c]);
+	}
+}
