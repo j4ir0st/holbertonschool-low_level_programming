cc7625cae1969888d333ea3c122749dd1030df5f
j4ir0st <jairocast2@gmail.com>
2021-11-03 13:15:13 -0700

    prints a name

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * print_name - prints a name
+ * @name: name of the person
+ * @f: - pointer function
+ * Return: Nothing.
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+	f(name);
+}
