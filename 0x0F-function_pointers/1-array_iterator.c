c60555110f0e83e8effef0d3a34cc47bbbef77c9
j4ir0st <jairocast2@gmail.com>
2021-11-03 13:44:40 -0700

    array iterator

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -12,11 +12,11 @@ void array_iterator(int *array, size_t size, void (*action)(int))
 {
 	size_t c;
 
-	if (array == NULL)
+	if (array == NULL || action == NULL)
 	{
 		return;
 	}
-	for(c = 0; c < size; c++)
+	for (c = 0; c < size; c++)
 	{
 		action(array[c]);
 	}
