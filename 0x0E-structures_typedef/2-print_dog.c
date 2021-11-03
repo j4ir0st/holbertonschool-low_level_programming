7df42c8db72206971e9c81d33eebd08a8e6995d3
j4ir0st <jairocast2@gmail.com>
2021-11-02 19:26:02 -0700

    prints dogs attributes

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -13,5 +13,22 @@ void print_dog(struct dog *d)
 	{
 		return;
 	}
-	printf("Name: %s\nAge: %f\nOwner: %s\n", d->name, d->age, d->owner);
+	if ( d->name == NULL)
+	{
+		printf("Name: (nil)\n");
+
+	}
+	else
+	{
+		printf("Name: %s\n", d->name);
+	}
+	printf("Age: %f\n", d->age);
+	if (d->owner == NULL)
+	{
+		printf("Owner: (nil)\n");
+	}
+	else
+	{
+		printf("Owner: %s\n", d->owner);
+	}
 }
