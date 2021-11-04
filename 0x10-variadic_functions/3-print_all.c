5d8e571916436d2ac135b75c25bd0049e2558275
j4ir0st <jairocast2@gmail.com>
2021-11-04 15:24:45 -0700

    prints strings, int, floats, chars

---
+++ 0x10-variadic_functions/3-print_all.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+#include <stdarg.h>
+
+/**
+ * print_all - check the code
+ * @format: const char
+ */
+
+void print_all(const char * const format, ...)
+{
+	va_list m;
+	char *sep = ", ";
+	int c = 0;
+	char *str;
+
+	va_start(m, format);
+	while (format != NULL && *(format + c) != '\0')
+	{
+		switch (*(format + c))
+		{
+		case 'c':
+			printf("%c", va_arg(m, int));
+			break;
+		case 'i':
+			printf("%i", va_arg(m, int));
+			break;
+		case 'f':
+			printf("%f", va_arg(m, double));
+			break;
+		case 's':
+			str = va_arg(m, char*);
+			if (str == NULL)
+			{
+				printf("nil");
+				break;
+			}
+			printf("%s", str);
+			break;
+		default:
+			c++;
+			continue;
+		}
+		c++;
+		if (*(format + c) != '\0')
+		{
+			printf("%s", sep);
+		}
+	}
+	printf("\n");
+	va_end(m);
+}
