b873b07b1e17e6850d7884e3c6e22f9c443376b1
j4ir0st <jairocast2@gmail.com>
2021-11-12 08:25:03 -0800

    Considering NULL

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -3,6 +3,7 @@
 /**
  * print_list - prints all elements of list
  * @h: list_t argument
+ * Return: number of nodes
  */
 
 size_t print_list(const list_t *h)
@@ -11,7 +12,14 @@ size_t print_list(const list_t *h)
 
 	while (h != NULL)
 	{
-		printf("[%u] %s\n", h->str, h->len);
+		if (h->str == NULL)
+		{
+			printf("[0] (nil)");
+		}
+		else
+		{
+			printf("[%u] %s\n", h->str, h->len);
+		}
 		h = h->next;
 		c++;
 	}
