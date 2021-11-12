fd3926d648786f5855819e09866c92f21d6802d8
j4ir0st <jairocast2@gmail.com>
2021-11-12 13:17:15 -0800

    hare and tortoise, attribute constructor, betty style

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
-
+/**
+ * before_main - prints a mesage before main
+ */
 void __attribute__ ((constructor)) before_main(void)
 {
 	printf("You're beat! and yet, you must allow,");
