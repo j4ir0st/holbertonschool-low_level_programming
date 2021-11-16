38dba56aaae75e23ee0a4ea4d98b5b23d8dae887
j4ir0st <jairocast2@gmail.com>
2021-11-16 09:50:11 -0800

    free the memory used by a linked list and sets the head to NULL

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+ * free_listint2 - frees the linked list and sets the head to NULL
+ * @head: the head of the linked list
+ */
+
+void free_listint2(listint_t **head)
+{
+	listint_t *tmp;
+
+	tmp = *head;
+	while (*head)
+	{
+		tmp = (*head)->next;
+		free(*head);
+		*head = tmp;
+	}
+	head = NULL;
+}
