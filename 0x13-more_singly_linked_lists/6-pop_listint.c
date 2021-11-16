be5b4f546eb539941b94e1706d47dcf684fb604d
j4ir0st <jairocast2@gmail.com>
2021-11-16 09:59:30 -0800

    adding return 0 if it's empty

--- 0x13-more_singly_linked_lists/6-pop_listint.c
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -3,7 +3,7 @@
 /**
  * pop_listint - deletes the head of a linked list
  * @head: head of the linked list
- * Return: the node data i
+ * Return: the node data i or 0 if it's empty
  */
 
 int pop_listint(listint_t **head)
@@ -11,6 +11,10 @@ int pop_listint(listint_t **head)
 	listint_t *tmp;
 	int i;
 
+	if (*head == NULL)
+	{
+		return (0);
+	}
 	i = (*head)->n;
 	tmp = (*head)->next;
 	free(*head);
