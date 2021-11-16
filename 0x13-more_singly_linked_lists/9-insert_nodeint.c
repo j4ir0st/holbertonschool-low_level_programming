7e0c6d8cfd63cd6afec12ef1635326dce774ef7a
j4ir0st <jairocast2@gmail.com>
2021-11-16 11:28:58 -0800

    considering if head is NULL

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -16,7 +16,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	tmp = *head;
 	while (c < idx)
 	{
-		if (tmp == NULL)
+		if (tmp->next == NULL || tmp == NULL)
 		{
 			return (NULL);
 		}
