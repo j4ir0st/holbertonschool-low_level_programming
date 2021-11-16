90b018868f2d52f447756f6aff0de0a9ceb8d2b2
j4ir0st <jairocast2@gmail.com>
2021-11-16 09:32:12 -0800

    adding header file lists.h

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -1,5 +1,45 @@
-#ifndef _MAIN_H_
-#define _MAIN_H_
+#ifndef _LISTS_H_
+#define _LISTS_H_
+
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+
+/**
+ * struct listint_s - singly linked list
+ * @n: integer
+ * @next: points to the next node
+ *
+ * Description: singly linked list node structure
+ */
+
+typedef struct listint_s
+{
+    int n;
+    struct listint_s *next;
+} listint_t;
+
+/**
+ * print_listint - prints all the elements of a list
+ * @h: contant listint_t argument
+ * Return: On success 0.
+ */
+
+listint_t *find_listint_loop(listint_t *head);
+size_t free_listint_safe(listint_t **h);
+size_t print_listint_safe(const listint_t *head);
+listint_t *reverse_listint(listint_t **head);
+int delete_nodeint_at_index(listint_t **head, unsigned int index);
+listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n);
+int sum_listint(listint_t *head);
+listint_t *get_nodeint_at_index(listint_t *head, unsigned int index);
+int pop_listint(listint_t **head);
+void free_listint2(listint_t **head);
+void free_listint(listint_t *head);
+listint_t *add_nodeint_end(listint_t **head, const int n);
+listint_t *add_nodeint(listint_t **head, const int n);
+size_t listint_len(const listint_t *h);
+size_t print_listint(const listint_t *h);
 
 /**
  * _putchar - writes the character c to stdout
