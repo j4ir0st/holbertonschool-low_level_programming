fc30b7b9969518461aff6b48c226d8bc0540206d
j4ir0st <jairocast2@gmail.com>
2021-11-16 12:29:14 -0800

    Adding current pointer

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -34,7 +34,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	{
 		new->next = *head;
 		*head = new;
-		return (*head);
+		return (new);
 	}
 	new->next = curr;
 	tmp->next = new;
