55452f91847ff58937944c9f8f3908b6f402d98f
j4ir0st <jairocast2@gmail.com>
2021-11-16 12:38:29 -0800

    Adding current pointer

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -16,7 +16,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	curr = *head;
 	while (c < idx)
 	{
-		if (tmp->next == NULL)
+		if (curr->next == NULL)
 		{
 			return (NULL);
 		}
@@ -25,7 +25,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		c++;
 	}
 	new = malloc(sizeof(listint_t));
-	if (new == NULL)
+	if (new == NULL || head == NULL)
 	{
 		return (NULL);
 	}
