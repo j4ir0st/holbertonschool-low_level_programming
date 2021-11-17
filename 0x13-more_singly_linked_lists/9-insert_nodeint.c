9336a702c28be980796a5b68c53b64c05e1c6c74
j4ir0st <jairocast2@gmail.com>
2021-11-16 16:51:48 -0800

    changing NULL options

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -33,9 +33,17 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	}
 	if (idx == 0)
 	{
-		new->next = *head;
-		*head = new;
-		return (*head);
+		if (head != NULL)
+		{
+			new->next = *head;
+			*head = new;
+			return (*head);
+		}
+		else
+		{
+			free(new);
+			return (NULL);
+		}
 	}
 	new->next = curr;
 	tmp->next = new;
