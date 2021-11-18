2304e207c07bc5f3db14f73d05dafefe1299b496
j4ir0st <jairocast2@gmail.com>
2021-11-18 13:54:51 -0800

    converts a binary into a decimal

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -6,18 +6,39 @@
  * Return: Integer
  */
 
+long potenciaConWhile(unsigned int numero, unsigned int potencia)
+{
+    unsigned int resultado = numero;
+    while (potencia > 1)
+    {
+        resultado = resultado * numero;
+        potencia--;
+    }
+    return resultado;
+}
+
+/**
+ * binary_to_uint - converts binary to int
+ * @b: binary as char
+ * Return: Integer
+ */
+
 unsigned int binary_to_uint(const char *b)
 {
 	int c = 0, i = 0;
 	unsigned int n = 0;
 
+	if (b == NULL)
+	{
+		return (0);
+	}
 	c = strlen(b) - 1;
 	while (c >= 0)
 	{
 		switch (*(b + c))
 		{
 		case '1':
-			n = n + pow(2, i);
+			n = n + potenciaConWhile(2, i);
 			i++;
 			break;
 		case '0':
