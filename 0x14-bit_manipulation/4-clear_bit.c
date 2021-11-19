b493d9e6ef0da92d799d63b308459d1e3a9623ee
j4ir0st <jairocast2@gmail.com>
2021-11-18 18:33:36 -0800

    sets to 0 de index value

---
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * set_bit - set to 1 the given bit
+ * @n: the given number
+ * @index: the bit to set
+ * Return: the new number
+ */
+
+int set_bit(unsigned long int *n, unsigned int index)
+{
+	if (index > sizeof(n) * 8)
+	{
+		return (-1);
+	}
+	index = 1 << index;
+	if (n < index)
+	{
+		*n = 0
+		return (-1);
+	}
+	*n -= index;
+	return (1);
+}
