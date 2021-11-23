a94a5ed34b5447d6a69cfeb7ee295bccf890f139
j4ir0st <jairocast2@gmail.com>
2021-11-22 19:36:58 -0800

    creates a file without change permissions

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -16,7 +16,7 @@ int create_file(const char *filename, char *text_content)
 	{
 		return (-1);
 	}
-	wd = write(fd, text_content, strlen(text_content));
+	wd = write(fd, text_content, strlen(text_content) + 1);
 	if (wd == -1)
 	{
 		close(fd);
