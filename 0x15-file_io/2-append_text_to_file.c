8d4e9ec9d2a7dc088c06d48295ffbeeee6a2af0a
j4ir0st <jairocast2@gmail.com>
2021-11-22 20:03:38 -0800

    appends text at the end of the file

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -16,10 +16,15 @@ int append_text_to_file(const char *filename, char *text_content)
 	{
 		return (-1);
 	}
-	if (text_content == NULL)
+	if (text_content == NULL && filename)
 	{
 		close(fd);
 		return (1);
+	}
+	else if (text_content == NULL && !filename)
+	{
+		close(fd);
+		return (-1);
 	}
 	wd = write(fd, text_content, strlen(text_content));
 	if (wd == -1)
