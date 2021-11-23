21750364f8c10a6cd13d7d1de04a9732c2fc5f06
j4ir0st <jairocast2@gmail.com>
2021-11-22 18:03:20 -0800

    read a text file and prints it to sdtout

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -19,7 +19,6 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	fd = open(filename, O_RDONLY);
 	if (fd == -1)
 	{
-		close(fd);
 		return (0);
 	}
 	buf = malloc(sizeof(char) * letters);
