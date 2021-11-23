8ee218ccf99ee7384ca2adddb8ad80b07530c462
j4ir0st <jairocast2@gmail.com>
2021-11-22 19:40:48 -0800

    creates a file considering content NULL

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -16,7 +16,12 @@ int create_file(const char *filename, char *text_content)
 	{
 		return (-1);
 	}
-	wd = write(fd, text_content, strlen(text_content) + 1);
+	if (text_context == NULL)
+	{
+		close(fd);
+		return (1);
+	}
+	wd = write(fd, text_content, strlen(text_content));
 	if (wd == -1)
 	{
 		close(fd);
