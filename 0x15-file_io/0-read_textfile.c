b5eaeca9616c7339740f432c50c2070e0302ea48
j4ir0st <jairocast2@gmail.com>
2021-11-22 18:18:44 -0800

    read a text file and prints it to sdtout with free write included

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -31,7 +31,7 @@ ssize_t read_textfile(const char *filename, size_t letters)
 		return (0);
 	}
 	wd = write(STDOUT_FILENO, buf, letters);
-	if (wd == -1 || strlen(filename) < letters)
+	if (wd == -1 || rd != wd)
 	{
 		free(buf);
 		close(fd);
