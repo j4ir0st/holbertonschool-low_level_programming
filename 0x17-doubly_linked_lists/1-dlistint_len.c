e6a38264f91483c95aac237725f635c40d86dc8b
j4ir0st <jairocast2@gmail.com>
2022-01-07 10:58:45 -0800

    Returns the number of elements of a linked list with betty style

--- 0x17-doubly_linked_lists/1-dlistint_len.c
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -1,4 +1,11 @@
 #include "lists.h"
+
+/**
+ * dlistint_len - check the code
+ * @h: head of the linked list
+ * Return: number of elemnts of linked list
+ */
+
 size_t dlistint_len(const dlistint_t *h)
 {
 	const dlistint_t *tmp = h;
