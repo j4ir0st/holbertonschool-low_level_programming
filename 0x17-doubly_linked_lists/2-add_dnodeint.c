69378f4d58b91a3480dd446e6d2a2d6af9bd98db
j4ir0st <jairocast2@gmail.com>
2022-01-17 19:47:07 -0800

    Adds a new node at the beginning with betty style

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -4,23 +4,24 @@
  * add_dnodeint - Prints the enviroment
  * @head: pointer of structure
  * @n: value of the new node
+ * Return: The address of the new node or NULL if it fails
  */
 
 dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 {
 	dlistint_t *new_st = NULL;
 
-        new_st = malloc(sizeof(dlistint_t));
+	new_st = malloc(sizeof(dlistint_t));
 	if (!new_st)
 		return (NULL);
 
-        if (*head)
-        {
-                (*head)->prev = new_st;
-        }
+	if (*head)
+	{
+		(*head)->prev = new_st;
+	}
 	new_st->n = n;
 	new_st->next = *head;
-        new_st->prev = NULL;
+	new_st->prev = NULL;
 	*head = new_st;
 	return (new_st);
 }
