5ec2d13434372b1b0512bc788161df555436e7e5
j4ir0st <jairocast2@gmail.com>
2022-01-28 12:34:34 -0800

    Creates a hash table using malloc

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -1,14 +1,24 @@
 #include "hash_tables.h"
 
+/**
+ * hash_table_create - check the code for
+ * @size: size of the hash table
+ * Return: Always EXIT_SUCCESS.
+ */
+
 hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *new_hashtable;
+	unsigned long int i;
 
 	new_hashtable = malloc(sizeof(hash_table_t));
 	if (new_hashtable == NULL)
 		return (NULL);
-	new_hashtable->array = malloc(sizeof(hash_node_t*) * size);
+	new_hashtable->array = malloc(sizeof(hash_node_t *) * size);
 	if (new_hashtable->array == NULL)
 		return (NULL);
+	new_hashtable->size = size;
+	for (i = 0; i < size; i++)
+		new_hashtable->array[i] = NULL;
 	return (new_hashtable);
 }
