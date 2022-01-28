ad5ab49759f04a2ddfcf097125a4322802a07f6d
j4ir0st <jairocast2@gmail.com>
2022-01-28 12:41:24 -0800

    Updating hash_table header

--- 0x1A-hash_tables/hash_tables.h
+++ 0x1A-hash_tables/hash_tables.h
@@ -1,5 +1,5 @@
-#ifndef _MAIN_H_
-#define _MAIN_H_
+#ifndef _HASHTABLES_H_
+#define _HASHTABLES_H_
 
 #include <stdlib.h>
 #include <string.h>
@@ -35,6 +35,7 @@ typedef struct hash_table_s
 	hash_node_t **array;
 } hash_table_t;
 
+hash_node_t *set_node(const char *key, const char *value);
 hash_table_t *hash_table_create(unsigned long int size);
 unsigned long int hash_djb2(const unsigned char *str);
 unsigned long int key_index(const unsigned char *key, unsigned long int size);
@@ -42,12 +43,6 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value);
 char *hash_table_get(const hash_table_t *ht, const char *key);
 void hash_table_print(const hash_table_t *ht);
 void hash_table_delete(hash_table_t *ht);
-/*shash_table_t *shash_table_create(unsigned long int size);
-int shash_table_set(shash_table_t *ht, const char *key, const char *value);
-char *shash_table_get(const shash_table_t *ht, const char *key);
-void shash_table_print(const shash_table_t *ht);
-void shash_table_print_rev(const shash_table_t *ht);
-void shash_table_delete(shash_table_t *ht);*/
 
 int _isupper(int c);
 int _isdigit(int c);
