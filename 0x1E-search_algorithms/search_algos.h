6944adbc2cfefa4c3588ea23d6a774cbde1d38a7
j4ir0st <jairocast2@gmail.com>
2022-06-16 11:38:56 -0500

    searches for a value in a sorted array of integers using the Interpolation search algorithm

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -10,6 +10,7 @@
 int linear_search(int *array, size_t size, int value);
 int binary_search(int *array, size_t size, int value);
 int jump_search(int *array, size_t size, int value);
+int interpolation_search(int *array, size_t size, int value);
 void print_array(int *array, int left, int right);
 
 #endif
